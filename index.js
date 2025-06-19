c036baf1d4fcc8ca8f0f9f8c4827922929d97738
Chehak Sharma <ch3hak@gmail.com>
2025-06-20 01:33:55 +0530

    register users

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const todos = require('./routes/Todo')
+const todos = require('./routes/Todo');
+const users = require('./routes/Users');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use('/api/todos', todos);
+app.use('/api/users', users);
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos')
     .then(() => console.log('connected to mongodb...'))
