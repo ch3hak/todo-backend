0fac312494b0fc0a71bc912b6a11a14e3779cd40
Chehak Sharma <ch3hak@gmail.com>
2025-06-19 18:54:54 +0530

    add delete endpoint

--- routes/Todo.js
+++ routes/Todo.js
@@ -16,10 +16,16 @@ router.put('/:id/toggle', async (req, res) => {
     }
 });
 
-module.exports = router;
-// router.delete('/:id', (req, res) => {
-//     const todo = todos.find(c => c.id === req.params.id)
-//     if(!todo) return res.status(404).send('Todo with given id does not exist')
+router.delete('/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id);
+        if(!todo) return res.status(404).send('Todo with given id does not exist')
 
-//     const deletedTodo = Todo.findByIdAndDelete(req.params.id);
-// })
+        const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+    }
+    catch (err) {
+        res.status(500).json({error: err.message});
+    }
+})
+
+module.exports = router;
