ea5f41d17ebfb7b9cae79ea15660fa768c9f4022
Chehak Sharma <ch3hak@gmail.com>
2025-06-19 18:26:02 +0530

    add put endpoint, structure files'

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const mongoose = require('mongoose');
-// const todos = require('./routes/Todo')
+const todos = require('./routes/Todo')
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use('/api/todos', todos);
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos')
     .then(() => console.log('connected to mongodb...'))
     .catch(err => console.error('couldnt connect...', err));
 
-const todoSchema = new mongoose.Schema({
-    title: String,
-    completed: {type: Boolean, default: false}
+app.listen(3000, () => {
+    console.log('Server is running on port 3000...');
 });
 
-const Todo = mongoose.model('Todo', todoSchema)
-
-async function createTodo(){
-    const todo = new Todo({
-        title: 'do leetcode'
-    });
+// async function createTodo(){
+//     const todo = new Todo({
+//         title: 'do leetcode'
+//     });
     
-    const result = await todo.save();
-    console.log(result);
-}
+//     const result = await todo.save();
+//     console.log(result);
+// }
 
-createTodo();
+// createTodo();
 
-// app.use('/api/todos', todos)
+// async function updateTodo(id){
+//     const result = await Todo.findByIdAndUpdate({_id: id}, {
+//         $set: {
+//             completed: true
+//         }
+//     });
+//     console.log(result);
+// }
+
+// updateTodo('68530ed67d71fff36d762794');
--- routes/Todo.js
+++ routes/Todo.js
@@ -1,16 +1,25 @@
-// const express = require('express')
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/todo');
 
-// router.put('/:id', (req, res) => {
-//     const todo = todos.find(c => c.id === req.params.id)
-//     if(!todo) return res.status(404).send('Todo with given id does not exist')
-//     todo.completed=!todo.completed;
-// });
+router.put('/:id/toggle', async (req, res) => {
+    try{ 
+        const todo = await Todo.findById(req.params.id);
+        if(!todo) return res.status(404).send('Todo with given id does not exist')
+        
+        todo.completed=!todo.completed;
+        const updatedTodo = await todo.save();
+        res.json(updatedTodo);
+    }
+    catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
 
+module.exports = router;
 // router.delete('/:id', (req, res) => {
 //     const todo = todos.find(c => c.id === req.params.id)
 //     if(!todo) return res.status(404).send('Todo with given id does not exist')
 
 //     const deletedTodo = Todo.findByIdAndDelete(req.params.id);
 // })
-// module.exports = router;
